df6fe7b684af7d7e86987322c4240ad05e84a736
Gerson Bernardo <gbds25@gmail.com>
2024-05-16 10:39:44 -0300

    Andamento Projeto 01 Parte 2

--- Projeto 01/routes/index.js
+++ Projeto 01/routes/index.js
@@ -1,20 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+/* GET home page. */ 
+router.get('/', async function (req, res) { 
+  try {
+    const results = await global.db.selectClientes(); 
+    console.log(results); 
+    res.render('index', { results }); 
+  } 
+  catch (error) { 
+    res.redirect('/?erro=' + error); 
+  } 
 });
 
-/* Get new page. */
-router.get('/new', function(req, res, next) {
-  res.render('new', { title: 'Cadastro de Clientes',action:"/new" });
+/* Get new page */
+router.get('/new',function(req,res,next) {
+  res.render('new', { title:"Cadastro de Cliente", action:"/new"});
 });
 
 /* POST new page. */
 router.post('/new', function(req, res, next) {
    //futuramente vamos salvar o cliente aqui
-   res.redirect('/?new=true')
-  })
-  
+   res.redirect('/?new=true');
+  });
+
 module.exports = router;
--- Projeto 01/routes/users.js
+++ Projeto 01/routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('Falta cadastrar usuários');
+  res.send('respond with a resource');
 });
 
 module.exports = router;
