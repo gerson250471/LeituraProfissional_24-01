d74a9205b991951a1647b0bc26ea62c1caf61840
Gerson Bernardo <gbds25@gmail.com>
2024-05-19 17:15:12 -0300

    Andamento do Projeto 01 Parte 5

--- Projeto 01/routes/index.js
+++ Projeto 01/routes/index.js
@@ -18,7 +18,6 @@ router.get('/new',function(req,res,next) {
   res.render('new', { title:"Cadastro de Cliente", result:{}, action:"/new"});
 });
 
-
 /* POST new page. */ 
 router.post('/new', async function (req, res) {
   const nome = req.body.nome  
@@ -45,6 +44,31 @@ router.get('/edit/:id', async function (req, res) {
     } 
   });
 
+/* POST edit page. */ 
+router.post('/edit/:id', async function (req, res) {
+  const id = parseInt(req.params.id); 
+  const nome = req.body.nome; 
+  const idade = !req.body.idade ? null : parseInt(req.body.idade); 
+  const uf = req.body.uf; 
+  try { 
+    await global.db.updateCliente(id, { nome, idade, uf }); 
+    res.redirect('/?edit=true') 
+  } 
+  catch (error) { 
+    res.redirect('/?erro=' + error); 
+  }
+ })
 
+/* GET delete page. */ 
+router.get('/delete/:id', async function (req, res) { 
+  const id = parseInt(req.params.ID); 
+  try {
+    await global.db.deleteCliente(id); 
+    res.redirect('/?delete=true'); 
+  } 
+  catch (error) {
+     res.redirect('/?erro=' + error); 
+    } 
+  })
 
 module.exports = router;
