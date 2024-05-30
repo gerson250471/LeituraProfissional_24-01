b350458d2ec2098f3ad87e7649f56802ff41fbc9
Gerson Bernardo <gbds25@gmail.com>
2024-05-30 11:25:20 -0300

    Estudo de Front-End Parte 04

--- Projeto 03/js/script.js
+++ Projeto 03/js/script.js
@@ -1,1 +1,35 @@
-console.log("Estou conectado")
+// Atualizar o display da calculadora
+function atualizarDisplay(btn){
+    const display = document.getElementById('display');
+    if (display.value.length === 9)
+        return;
+    if(display.value === '0') {
+        display.value = btn.value;
+    } else {
+        display.value += btn.value;
+    }
+}
+
+// Limpando Display
+function limparDisplay(){
+   document.getElementById('display').value=0;
+}
+
+// Guardando a operação que foi teclada
+let operador='';
+let valor1=0;
+function atualizarOperacao(btn){
+    const display = document.getElementById('display');
+    operador = btn.value;
+    valor1 = parseInt(display.value);
+    display.value ='0';
+}
+
+// Realizando o cálculo
+function calcularOperacao(){
+    const display = document.getElementById('display');
+    const valor2 = parseInt(display.value);
+    const total = eval(valor1 + operador + valor2);
+    display.value=total;
+    operador='';
+}
