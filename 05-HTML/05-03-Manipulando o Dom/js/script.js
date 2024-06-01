7d61e6e805a5def567cb26221ef7e3eb756f102f
Gerson Bernardo <gbds25@gmail.com>
2024-06-01 12:17:34 -0300

    Estudo de Front-End Parte 06

--- 05-HTML/05-03-Manipulando o Dom/js/script.js
+++ 05-HTML/05-03-Manipulando o Dom/js/script.js
@@ -1,1 +1,30 @@
-// document.getElementById(id);
+document.addEventListener("DOMContentLoaded",function(event){
+    /*  Esconder a Div listagem*/
+    const divListagem = document.getElementById("divListagem");
+    divListagem.style.display = "none";
+
+    const divCadastro = document.getElementById("divCadastro");
+
+    document.getElementById("btnListar").onclick = (evt) =>{
+        divListagem.style.display = "block";
+        divCadastro.style.display = "none";
+    }
+
+    document.getElementById("btnCadastrar").onclick = (evt) => {
+        divListagem.style.display = "none";
+        divCadastro.style.display = "block";
+    }
+
+    const frmCadastro = document.getElementById("frmCadastro");
+
+    frmCadastro.onsubmit = (evt) => {
+        let linha ='<tr>';
+        var data = new FormData(frmCadastro)
+
+        for(let item of data)
+            linha += `<td>${item[1]}</td>`;
+            linha += '<td><input type=button" value="X"/></td></tr>';
+            // se tem apenas uma TD, é a default
+    }
+
+});
