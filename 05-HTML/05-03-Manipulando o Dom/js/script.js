635d339e518225db673426971f88daaa29e1adfc
Gerson Bernardo <gbds25@gmail.com>
2024-06-03 10:48:36 -0300

    Estudo de Front-End Parte 07

--- 05-HTML/05-03-Manipulando o Dom/js/script.js
+++ 05-HTML/05-03-Manipulando o Dom/js/script.js
@@ -21,10 +21,32 @@ document.addEventListener("DOMContentLoaded",function(event){
         let linha ='<tr>';
         var data = new FormData(frmCadastro)
 
+        console.log("entrei")
+
         for(let item of data)
             linha += `<td>${item[1]}</td>`;
             linha += '<td><input type=button" value="X"/></td></tr>';
             // se tem apenas uma TD, é a default
-    }
+            const tbody = document.querySelector('table > tbody');
+            if (tbody.querySelectorAll('tr > td').length === 1)
+                tbody.innerHTML="";
 
+            tbody.innerHTML += linha;
+
+            divListagem.style.display = "block";
+            divCadastro.style.display = "none";
+
+            frmCadastro.reset();
+
+            const buttons = document.querySelectorAll("input[value='X']");
+            for(let btn of buttons){
+                if(btn.onclick !== null) continue;
+                btn.onclick = (evt) => {
+                    if (confirm("Tem certeza que deseja escluir este cliente?")){
+                        btn.closest('tr').remove();
+                    }
+                }
+            }
+            evt.preventDefault();
+    }
 });
